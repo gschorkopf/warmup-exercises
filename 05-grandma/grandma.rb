255f019fb63d8b4095640140e3971e090a6a0062
Geoff Schorkopf <gschorkopf@gmail.com>
2013-02-18 09:22:57 -0700

    added grandma exercise

---
+++ 05-grandma/grandma.rb
@@ -0,0 +1,26 @@
+class Grandma
+  def convo
+    command = ""
+    while command != "BYE"
+      printf "> "
+      command = gets.chomp
+      puts response_from(command)
+    end
+  end
+
+  def response_from(command)
+    if command == "BYE"
+        "OHBAI."
+    elsif command == command.upcase
+      "NO, NOT SINCE #{year}"
+    else
+      "HUH?!  SPEAK UP, SONNY!"
+    end
+  end
+
+  def year
+    rand(1930..1950)
+  end
+end
+
+Grandma.new.convo
